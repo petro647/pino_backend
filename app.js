374cb0eb16a3c630cd5475f3c1e6a3681cc2b8f5
petro647 <petrosinoa096@gmail.com>
2023-12-26 11:20:52 +0100

    dived in function

--- app.js
+++ app.js
@@ -7,63 +7,57 @@ require('dotenv').config();
 
 const login = require('./routes/login/signin');
 
-const app = express();
+function main(){
+  const app = express();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `${process.env.DB_CONNECTION_STRING}`;
+  setViewEngine(app);
+  setLocals(app);
+  setMiddlewares(app);
+  setRoutes(app);
+  errorHandler(app);
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-connectDB().catch(console.dir);
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// middlewares
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// routes
-app.use('/login/signin', login);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.send('<h1>Error</h1>')
-});
-
-module.exports = app;
-
-async function connectDB() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-  }
+  module.exports = app;
 }
 
-client.close();
+function setViewEngine(app){
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+}
+
+function setLocals(app){
+  app.use(function(req, res, next) {
+    next();
+  })
+}
+
+function setMiddlewares(app){
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+}
+
+function setRoutes(app){
+  app.use('/login/signin', login);
+}
+
+function errorHandler(app){
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    next(createError(404));
+  });
+
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.send('<h1>Error</h1>')
+  });
+}
+
+main();
